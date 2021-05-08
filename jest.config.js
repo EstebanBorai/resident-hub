61f6656699a3658964c7bfac2df14dd4d5b24777
Esteban Borai <estebanborai@gmail.com>
2021-05-08 15:07:50 -0400

    feat: impl basic auth utility

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '(/test/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
