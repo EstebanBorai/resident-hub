db1171b32e215e7cb5fc6015f8184fda6d4b9dd6
David Arenas <davejs136@gmail.com>
2021-05-31 08:48:56 -0400

    add: driver to services plugin

--- src/server/plugins/services.ts
+++ src/server/plugins/services.ts
@@ -5,6 +5,7 @@ import LoggerService from '../service/logger';
 import UserService from '../service/user';
 import ParkingLotService from '../service/parking-lot';
 import ParkingSlotService from '../service/parking-slot';
+import DriverService from '../service/driver';
 
 import type { FastifyInstance, RegisterOptions } from 'fastify';
 import type { IAuthService } from '../service/auth';
@@ -12,6 +13,7 @@ import type { ILoggerService } from '../service/logger';
 import type { IUserService } from '../service/user';
 import type { IParkingLotService } from '../service/parking-lot';
 import type { IParkingSlotService } from '../service/parking-slot';
+import type { IDriverService } from '../service/driver';
 
 export type Services = {
   auth: IAuthService;
@@ -19,6 +21,7 @@ export type Services = {
   user: IUserService;
   parkingLot: IParkingLotService;
   parkingSlot: IParkingSlotService;
+  driver: IDriverService;
 };
 
 export default fp(
@@ -32,6 +35,7 @@ export default fp(
     const auth = new AuthService(logger, user);
     const parkingLot = new ParkingLotService();
     const parkingSlot = new ParkingSlotService(parkingLot);
+    const driver = new DriverService(user);
 
     fastify.decorate('services', {
       auth,
@@ -39,6 +43,7 @@ export default fp(
       user,
       parkingLot,
       parkingSlot,
+      driver,
     });
 
     next();
